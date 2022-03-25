d68283c2841d6586dcddb2ae86707997fbd61079
Peter <pete.blinov@yandex.ru>
2022-03-26 01:39:00 +0300

    add table view

---
+++ src/components/ViewTable.vue
@@ -0,0 +1,33 @@
+<script setup>
+import keys from '@/keys'
+
+defineProps({
+  data: Array,
+})
+</script>
+
+<template lang="pug">
+table
+  thead
+    tr.theader
+      th(v-for="k in keys" :class="k").key {{k}}
+  tbody
+    tr(v-for="item in data").entry
+      td(v-for="k in keys") {{item[k]}}
+</template>
+
+<style scoped>
+thead {
+  background-color: var(--gh-black);
+  color: #fff;
+}
+
+th {
+  padding: 8px 12px;
+}
+
+.entry td {
+  background-color: #fff;
+  padding: 5px;
+}
+</style>
