87a4a25928f852c0ee9715a3521a313b30a48e61
Peter <perhwillhelm@hotmail.com>
2022-04-03 00:15:08 +0300

    add util string capitalizer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,6 @@
 export { formatTimeAgo } from './date'
 export { kFormatNumber, spaceFormatNumber } from './number'
+
+export function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1)
+}
